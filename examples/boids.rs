ccdc95d02c10cf86538dbce852d46a55237a054e
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2020-12-03 11:36:46 -0500

    quadtree

--- examples/boids.rs
+++ examples/boids.rs
@@ -1,5 +1,6 @@
 use nannou::prelude::*;
 use nannou::Draw;
+use sketches::quadtree::*;
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -9,6 +10,7 @@ struct Model {
     boids: Vec<Boid>,
 }
 
+#[derive(Clone, PartialEq)]
 struct Boid {
     position: Vector2,
     velocity: Vector2,
@@ -18,13 +20,19 @@ struct Boid {
     max_speed: f32,
 }
 
+impl Position for Boid {
+    fn pos(&self) -> Point2 {
+        self.position
+    }
+}
+
 impl Boid {
     fn new(x: f32, y: f32) -> Self {
         let position = vec2(x, y);
         let velocity = vec2(random_range(-1.0, 1.0), random_range(-1.0, 1.0)).with_magnitude(4.0);
         let acceleration = vec2(0.0, 0.0);
-        let r = 5.0;
-        let max_force = 0.16;
+        let r = 3.0;
+        let max_force = 1.0;
         let max_speed = 4.0;
 
         Boid {
@@ -45,19 +53,22 @@ impl Boid {
         steer: impl Fn(Vector2, i32) -> Vector2,
     ) -> Vector2 {
         let mut sum = vec2(0., 0.);
-        let mut count = 0;
+        let mut quad_tree = QNode::Points(vec![]);
         for b in boids {
-            let d = self.position.distance(b.position);
-            if d > 0. && d < dist {
-                sum += acc(b);
-                count += 1;
+            if b != self {
+                quad_tree.insert(b.clone(), vec2(-750.0, -500.0), vec2(750.0, 500.0));
             }
         }
-        if count > 0 {
-            return steer(sum, count);
-        } else {
-            return vec2(0., 0.);
+        let flock =
+            quad_tree.points_in_circle(vec2(-750.0, -500.0), vec2(750.0, 500.0), self.pos(), dist);
+        let n = flock.len();
+        for ref b in flock {
+            sum += acc(b);
         }
+        if n > 0 {
+            return steer(sum, n as i32);
+        }
+        vec2(0., 0.)
     }
 
     fn align(&self, boids: &Vec<Boid>, dist: f32) -> Vector2 {
@@ -117,11 +128,15 @@ impl Boid {
 }
 
 fn model(app: &App) -> Model {
-    app.new_window().size(1500, 1000).view(view).build().unwrap();
+    app.new_window()
+        .size(1500, 1000)
+        .view(view)
+        .build()
+        .unwrap();
     let mut boids = Vec::new();
-    for _ in 0..200 {
-        let x = random_range(-50., 50.);
-        let y = random_range(-50., 50.);
+    for _ in 0..1000 {
+        let x = random_range(-750., 750.);
+        let y = random_range(-500., 500.);
         boids.push(Boid::new(x, y));
     }
     Model { boids }
@@ -133,7 +148,7 @@ fn update(app: &App, m: &mut Model, _update: Update) {
     let mut coh = Vec::new();
     for boid in &m.boids {
         sep.push(boid.separate(&m.boids, 50.) * 1.2);
-        ali.push(boid.align(&m.boids, 100.));
+        ali.push(boid.align(&m.boids, 50.));
         coh.push(boid.cohesion(&m.boids, 100.));
     }
     for (i, boid) in m.boids.iter_mut().enumerate() {
